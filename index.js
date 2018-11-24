7cafe685b1a1ab89d5877f87833d28a34a411b62
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-11-24 21:32:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var recipes = {};
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
 
 function updateObjectWithKeyAndValue(object, key, value){
   var Copy = Object.assign({}, object, {[key]: value});
