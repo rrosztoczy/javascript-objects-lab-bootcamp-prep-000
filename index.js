2e5eaf702599c60b2e2156ce305c29025d155a38
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-11-24 21:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+ object[key] = value 
+ return object
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
