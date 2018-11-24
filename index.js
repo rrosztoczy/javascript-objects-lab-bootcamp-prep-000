391b8b761d2858fbd09868a2aed3a4d7312565a2
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-11-24 21:40:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var deleteCopy = delete object[key];
+  var deleteCopy = Object.assign({}, object);
+  delete deleteCopy[key];
   return deleteCopy
 }
